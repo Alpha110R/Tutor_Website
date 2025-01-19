37762809fbbc42710ec5cb94f51c16d68c838e56
Alon Ronder <alonr110@gmail.com>
2025-01-19 21:51:01 +0200

    move all css to global.css file

--- src/app/page.js
+++ src/app/page.js
@@ -18,34 +18,30 @@ export default function Home() {
       <Image
         src={bg}
         priority
-        sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+        sizes="100vw"
         alt="Coding Wizards School page background image"
         className="-z-50 fixed w-full h-full object-cover object-center opacity-60"
         loading="eager"
       />
 
-      <div className="relative w-full h-screen flex flex-col items-center justify-center">
-        <div className="absolute top-[20%] text-center">
-          <h1 className="font-bold text-accent text-3xl xs:text-5xl sm:text-6xl lg:text-7xl">
-            Coding Wizards School
-          </h1>
-        </div>
-
-        <div className="absolute bottom-0 w-full h-3/4 flex justify-center">
-          <RenderModel>
-            <Wizard />
-          </RenderModel>
+      <div className="relative w-full h-screen flex items-center justify-center">
+        <div className="hero-title-wrapper">
+          <h1 className="title">Coding Wizards School</h1>
         </div>
       </div>
 
-      <div className="flex min-h-screen flex-col items-center justify-between relative">
-        <div className="w-full h-screen">
+      <div className="absolute bottom-0 w-full h-3/4 flex justify-center">
+        <RenderModel>
+          <Wizard />
+        </RenderModel>
+      </div>
+
+      <div className="section-wrapper">
+        <div className="section-content">
           <About />
-          <div className="w-full flex justify-center items-center py-20">
-            <div className="max-w-5xl w-full px-8 flex flex-col items-center">
-              <h2 className="text-4xl font-bold text-center text-yellow-300 mb-10">
-                צרו קשר
-              </h2>
+          <div className="content-wrapper">
+            <div className="content-container">
+              <h2 className="contact-title">צרו קשר</h2>
               <div className="w-full flex justify-center">
                 <Form />
               </div>
